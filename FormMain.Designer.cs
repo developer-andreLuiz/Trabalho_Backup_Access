e9db4faeaba4dd2ffee2d14d1ce50e0be37bbc09
André Luiz <developer.andreluiz@gmail.com>
2021-03-17 18:10:44 -0300

    Projeto Carregado

---
+++ FormMain.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace SistemaBackupAccess
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnCopiar = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblHora = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnCopiar
+            // 
+            this.btnCopiar.Location = new System.Drawing.Point(50, 51);
+            this.btnCopiar.Name = "btnCopiar";
+            this.btnCopiar.Size = new System.Drawing.Size(147, 40);
+            this.btnCopiar.TabIndex = 0;
+            this.btnCopiar.Text = "Iniciar com Windows";
+            this.btnCopiar.UseVisualStyleBackColor = true;
+            this.btnCopiar.Click += new System.EventHandler(this.btnCopiar_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblHora
+            // 
+            this.lblHora.AutoSize = true;
+            this.lblHora.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHora.Location = new System.Drawing.Point(92, 18);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(51, 20);
+            this.lblHora.TabIndex = 1;
+            this.lblHora.Text = "label1";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 2000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(246, 103);
+            this.Controls.Add(this.lblHora);
+            this.Controls.Add(this.btnCopiar);
+            this.Name = "FormMain";
+            this.Text = "Backup Access";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCopiar;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Win32;
+
+namespace SistemaBackupAccess
+{
+    public partial class FormMain : Form
+    {
+        bool a = true;
+       
+        //string localOriginal = @"C:\Arquivos Sistema Valendo\Banco_Dados\Soft.mdb";
+        //string localCopia= @"C:\Users\MAINSERVIDOR\Google Drive\Banco de Dados\Soft.mdb";
+        string localOriginal = @"C:\Users\devel\Desktop\t1\Soft.mdb";
+        string localCopia = @"C:\Users\devel\Desktop\t2\Soft.mdb";
+
+        public FormMain()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void btnCopiar_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                RegistryKey reg = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+                reg.SetValue("Sistema Backup Access", Application.ExecutablePath.ToString());
+            }
+            catch(Exception a)
+            {
+                MessageBox.Show(a.Message);
+            }
+           
+
+
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblHora.Text = DateTime.Now.ToString("HH:mm:ss");
+            if (lblHora.Text.Equals("06:00:00"))
+            {
+                try
+                {
+                    File.Copy(localOriginal, localCopia, true);
+                }
+                catch (Exception a)
+                {
+                    MessageBox.Show(a.Message);
+                }
+
+            }
+            if (lblHora.Text.Equals("08:00:00"))
+            {
+                try
+                {
+                    File.Copy(localOriginal, localCopia, true);
+                }
+                catch (Exception a)
+                {
+                    MessageBox.Show(a.Message);
+                }
+
+            }
+            if (lblHora.Text.Equals("09:00:00"))
+            {
+                try
+                {
+                    File.Copy(localOriginal, localCopia, true);
+                }
+                catch (Exception a)
+                {
+                    MessageBox.Show(a.Message);
+                }
+
+            }
+            if (lblHora.Text.Equals("12:00:00"))
+            {
+                try
+                {
+                    File.Copy(localOriginal, localCopia, true);
+                }
+                catch (Exception a)
+                {
+                    MessageBox.Show(a.Message);
+                }
+
+            }
+            if (lblHora.Text.Equals("15:00:00"))
+            {
+                try
+                {
+                    File.Copy(localOriginal, localCopia, true);
+                }
+                catch (Exception a)
+                {
+                    MessageBox.Show(a.Message);
+                }
+
+            }
+            if (lblHora.Text.Equals("18:00:00"))
+            {
+                try
+                {
+                    File.Copy(localOriginal, localCopia, true);
+                }
+                catch (Exception a)
+                {
+                    MessageBox.Show(a.Message);
+                }
+
+            }
+            if (lblHora.Text.Equals("20:00:00"))
+            {
+                try
+                {
+                    File.Copy(localOriginal, localCopia, true);
+                }
+                catch (Exception a)
+                {
+                    MessageBox.Show(a.Message);
+                }
+
+            }
+            if (lblHora.Text.Equals("21:00:00"))
+            {
+                try
+                {
+                    File.Copy(localOriginal, localCopia, true);
+                }
+                catch (Exception a)
+                {
+                    MessageBox.Show(a.Message);
+                }
+
+            }
+            if (lblHora.Text.Equals("23:00:00"))
+            {
+                try
+                {
+                    File.Copy(localOriginal, localCopia, true);
+                }
+                catch (Exception a)
+                {
+                    MessageBox.Show(a.Message);
+                }
+
+            }
+        }
+
+       
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (a == true)
+            {
+                this.Hide();
+                a = false;
+            }
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                File.Copy(localOriginal, localCopia, true);
+            }
+            catch (Exception a)
+            {
+                MessageBox.Show(a.Message);
+            }
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                File.Copy(localOriginal, localCopia, true);
+            }
+            catch (Exception a)
+            {
+                MessageBox.Show(a.Message);
+            }
+        }
+    }
+}
